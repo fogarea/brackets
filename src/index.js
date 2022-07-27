fc496abbcdd2d233eac80a4d774fb9d93feeb7c6
fogarea <shiitakedd@gmail.com>
2022-07-27 23:48:30 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  let brackets = {};
+  for (let index of bracketsConfig) {
+    brackets[index[1]] = index[0]
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    arr.length === 0 ? arr.push(str[i]) : (brackets[str[i]] === arr[arr.length - 1] ? arr.pop() : arr.push(str[i]))
+  }
+
+  return !arr.length;
 }
